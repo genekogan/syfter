e8fc4bf2220e574eba95698c50ad07648713f93f
genekogan <kogan.gene@gmail.com>
2019-11-04 23:12:24 -0500

    global model

--- app.py
+++ app.py
@@ -20,9 +20,41 @@ app = Flask(__name__)
 
 is_running = False
 
+model = None
+
+
+class VAE(nn.Module):
+    def __init__(self, image_size=784, h_dim=400, z_dim=20):
+        super(VAE, self).__init__()
+        self.fc1 = nn.Linear(image_size, h_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(h_dim, z_dim)
+        self.fc4 = nn.Linear(z_dim, h_dim)
+        self.fc5 = nn.Linear(h_dim, image_size)
+        
+    def encode(self, x):
+        h = F.relu(self.fc1(x))
+        return self.fc2(h), self.fc3(h)
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(log_var/2)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, z):
+        h = F.relu(self.fc4(z))
+        return F.sigmoid(self.fc5(h))
+    
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        x_reconst = self.decode(z)
+        return x_reconst, mu, log_var
+
 
 
 def run_me(x, s):
+    global model    
     print("going to thread here...", x, s)
     global is_running
     
@@ -59,38 +91,6 @@ def run_me(x, s):
                     train=True, download=True, transform=transforms.ToTensor())
         .federate((bob, alice)), 
         batch_size=batch_size, shuffle=True, **kwargs)
-
-
-    class VAE(nn.Module):
-        def __init__(self, image_size=784, h_dim=400, z_dim=20):
-            super(VAE, self).__init__()
-            self.fc1 = nn.Linear(image_size, h_dim)
-            self.fc2 = nn.Linear(h_dim, z_dim)
-            self.fc3 = nn.Linear(h_dim, z_dim)
-            self.fc4 = nn.Linear(z_dim, h_dim)
-            self.fc5 = nn.Linear(h_dim, image_size)
-            
-        def encode(self, x):
-            h = F.relu(self.fc1(x))
-            return self.fc2(h), self.fc3(h)
-
-        def reparameterize(self, mu, log_var):
-            std = torch.exp(log_var/2)
-            eps = torch.randn_like(std)
-            return mu + eps * std
-
-        def decode(self, z):
-            h = F.relu(self.fc4(z))
-            return F.sigmoid(self.fc5(h))
-        
-        def forward(self, x):
-            mu, log_var = self.encode(x)
-            z = self.reparameterize(mu, log_var)
-            x_reconst = self.decode(z)
-            return x_reconst, mu, log_var
-
-
-
         
     model = VAE().to(device)
 
